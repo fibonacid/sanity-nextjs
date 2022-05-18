74bab0e028f83ff5940c728d8fce74fc1e2b7240
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-05-18 11:41:32 +0200

    configure sanity production routing rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/studio/:path*",
+        destination: "/studio/index.html", // The :path parameter is used here so will not be automatically passed in the query
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
